c6fc7f73b10366f9881707927fe2867fabf2ddb7
Rilr20 <nrrl99@gmail.com>
2022-12-04 13:36:00 +0100

    part 2 done

--- day4/day4/Program.cs
+++ day4/day4/Program.cs
@@ -14,38 +14,65 @@ namespace day4
         {
             string filePath = Path.GetFullPath("..\\..\\input.txt");
             string[] fileContents = File.ReadAllLines(filePath);
-
             int InRangeCount = 0;
-            string[] ranges = new string[2];
-            bool result = false;
+            int part2Count = 0;
+            string[] ranges;
+            bool result;
+
             foreach (String line in fileContents)
             {
                 ranges = line.Split(',');
                 result = formatter(ranges[0], ranges[1]);
-                InRangeCount = result ? InRangeCount+=1 : InRangeCount;
-            }
-            Console.WriteLine(InRangeCount);
-            Console.ReadLine();
+                InRangeCount = result ? InRangeCount += 1 : InRangeCount;
 
+                result = overlaps(ranges[0], ranges[1]);
+                part2Count = result ? part2Count += 1 : part2Count;
+
+            }
+            Console.WriteLine(InRangeCount + "\n");
+            Console.WriteLine(part2Count);
+            Console.ReadLine();
         }
         static bool formatter(string range1, string  range2)
         {
             string[] range1Split = new string[2];
             string[] range2Split = new string[2];
+            int[] firstRangeArr = new int[2];
+            int[] secondRangeArr = new int[2];
+
             range1Split = range1.Split('-');
             range2Split = range2.Split('-');
-            int[] convertedNumbers = new int[2];
-            convertedNumbers[0] = Convert.ToInt32(range2Split[0]);
-            convertedNumbers[1] = Convert.ToInt32(range2Split[1]);
-            return InsideRange(Convert.ToInt32(range1Split[0]), Convert.ToInt32(range1Split[1]), convertedNumbers);
-            
+
+            firstRangeArr[0] = Convert.ToInt32(range1Split[0]);
+            firstRangeArr[1] = Convert.ToInt32(range1Split[1]);
+
+            secondRangeArr[0] = Convert.ToInt32(range2Split[0]);
+            secondRangeArr[1] = Convert.ToInt32(range2Split[1]);
+            return InsideRange(firstRangeArr, secondRangeArr);
         }
-        static bool InsideRange(int startPos, int EndPos, int[] range)
+        static bool InsideRange(int[] range1, int[] range2)
         {
-            int[] createdRange = Enumerable.Range(startPos, EndPos-startPos+1).ToArray();
-            int[] createdRange2 = Enumerable.Range(range[0], range[1]-range[0]+1).ToArray();
-            return (createdRange.Contains(range[0]) && createdRange.Contains(range[1])
-                    || createdRange2.Contains(startPos) && createdRange2.Contains(EndPos));
+            return range1[0] <= range2[0] && range1[1] >= range2[1] ||
+                    range2[0] <= range1[0] && range2[1] >= range1[1];
+        }
+        static bool overlaps(string firstRange, string secondRange)
+        {
+            string[] range1Split = new string[2];
+            string[] range2Split = new string[2];
+            int[] firstRangeArr = new int[2];
+            int[] secondRangeArr = new int[2];
+
+            range1Split = firstRange.Split('-');
+            range2Split = secondRange.Split('-');
+            
+            firstRangeArr[0] = Convert.ToInt32(range1Split[0]);
+            firstRangeArr[1] = Convert.ToInt32(range1Split[1]);
+            
+            secondRangeArr[0] = Convert.ToInt32(range2Split[0]);
+            secondRangeArr[1] = Convert.ToInt32(range2Split[1]);
+
+            return (firstRangeArr[1] >= secondRangeArr[0] && firstRangeArr[1] <= secondRangeArr[1]) ||
+                    (secondRangeArr[1] >= firstRangeArr[0] && secondRangeArr[1] <= firstRangeArr[1]);
         }
     }
 }
