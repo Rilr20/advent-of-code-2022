202983f78c0171addda0e86058e8e9665fb6c807
Rilr20 <nrrl99@gmail.com>
2022-12-04 14:01:03 +0100

    day 1 rust part 1, part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,37 +1,44 @@
 // rustc compiler
 // run by ./filename
 
-// use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-    let mut calories:Vec<&u32> = Vec::new();
+    let mut calories:Vec<u32> = Vec::new();
     let file_path = "../input.txt";
-    println!("In file {}", file_path);
 
     let mut file = File::open(file_path).expect("Can't Open File!");
-
     let mut contents = String::new();
 
     file.read_to_string(&mut contents)
         .expect("Oops can't read file sadface :(");
 
-    let split = contents.lines();
+    let split = contents.split("\n\n");
     let elves: Vec<&str> = split.collect();
 
-    let mut current_calories: u32 = 0;
-    for calorie in elves {
-        if calorie != "" {
-            println!("{}", calorie);
-            *&mut current_calories = &current_calories + &calorie.parse::<u32>().unwrap();
-        } else {
-            print!("empty row\n");
-            calories.push(&current_calories);
-            *&mut current_calories = 0;
-        }
+    for calorie_list in elves {
+        calories.push(sum_all_items(calorie_list));
     }
 
-    println!("{:?}", calories);
+    calories.sort_by(|a, b| b.cmp(a));
+    println!("{}", calories[0]);
+    let mut top_three_sum = 0;
+    for i in 0..3 {
+        top_three_sum += calories[i];
+    }
+    println!("{}", calories[0]);
+    println!("{}", top_three_sum);
+}
+
+fn sum_all_items(items:&str) -> u32 {
+    let split = items.split("\n");
+    let itemvector: Vec<&str> = split.collect();
+    let mut sum_of_items = 0; 
+    for item in itemvector {
+        let converted_to_int: u32 = item.parse().unwrap();
+        sum_of_items += converted_to_int;
+    }
+    return sum_of_items
 }
 
