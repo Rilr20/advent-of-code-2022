594d973163d20e5b145077f9662c2270cb554e6f
Rilr20 <nrrl99@gmail.com>
2024-05-15 17:26:50 +0200

    returns too large value

--- day7/ts/main.ts
+++ day7/ts/main.ts
@@ -5,67 +5,66 @@ interface DirectoryHashmap {
 export default {
     part1(file: string): number {
         // Implementation
+
         let file_split = file.split("\n")
         let current_directory = ""
         let directory_hashmap: DirectoryHashmap = {}
+        let result = 0
         for (let i = 0; i < file_split.length; i++) {
-            const element = file_split[i];
-            const split_element = element.split(" ")
-            if (element.indexOf("cd") !== -1) {
-                if (element.indexOf('..') !== -1 && element !== "$ cd /") {
+            let split_line = file_split[i].split(" ")
 
-                    let split = current_directory.split("/")
-                    split = split.splice(0, split.length - 2)
-
-                    current_directory = split.join("/") + "/"
-
-
-                } else if (element === "$ cd /") {
-                    current_directory = "/"
+            if (split_line[0] === "$" && split_line[1] === "cd") {
+                if (split_line[2] === "..") {
+                    let find = current_directory.match(/^(.*)\//);
+                    if (find !== null) {
+                        if (find[1] == "") {
+                            current_directory = "/"
+                        } else {
+                            current_directory = find[1]
+                        }
+                    }
                 } else {
-
-                    current_directory += element.split(" ")[2] + "/"
+                    if (current_directory == "/" || current_directory === "") {
+                        current_directory = current_directory + split_line[2]
+                    } else {
+                        current_directory = current_directory + "/" + split_line[2]
+                    }
                 }
             }
-            if (!Number.isNaN(Number(split_element[0]))) {
-                // console.log(Number(split_element[0]));
-                if (directory_hashmap[current_directory] === undefined) {
 
-                    directory_hashmap[current_directory] = 0
+            if (!isNaN(parseInt(split_line[0]))) {
+                let directories = current_directory.match(/([a-z]*)/g)
+                if (directories !== null) {
+
+                    for (let i = 0; i < directories.length; i++) {
+                        if (directories[i] !== '' || directories[i] === '' && i === 1) {
+                            let path = "/"
+                            directories.slice(0, i+1).forEach(item => {
+                                if (item !== '') {
+                                    if (path === "/") {
+                                        path += item
+                                    } else {
+                                        path += "/" + item
+                                    }
+                                }
+                            })
+                            if (directory_hashmap[path] === undefined) {
+                                directory_hashmap[path] = 0
+                            }
+                            directory_hashmap[path] += parseInt(split_line[0])
+                        }
+                    }
                 }
-                directory_hashmap[current_directory] += Number(split_element[0])
             }
-
+         }
         }
-        console.log(directory_hashmap);
-        
-
-        // Calculate upper directory size.
-        Object.entries(directory_hashmap).forEach(item => {
-            const [key, value] = item
-            // console.log(key);
-            Object.entries(directory_hashmap).forEach(item2 => {
-                const [key2, value2] = item2
-                // console.log(item);
-                if (key !== key2 && key2.indexOf(key) !== -1) {
-                    directory_hashmap[key] += value2
-                }
-
-
-
-            })
+   
+        let values = Object.values(directory_hashmap)
+        values = values.filter(item => {
+            return item < 100000
         })
-
-        let result = 0 
-        Object.entries(directory_hashmap).forEach(item => {
-            const [key, value] = item
-            if (value < 100000) {
-                result += value
-            }
-        })
-        // console.log(filteredMap);
         
-        return result
+        return values.reduce((a,b) => a+b, 0)
     },
     part2(file: string): number {
         // Implementation
