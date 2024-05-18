7069bf7db57943a78b50d78193ae18b1b5cbab30
Rilr20 <nrrl99@gmail.com>
2024-05-18 19:38:36 +0200

    Part one completed

--- day8/ts/main.ts
+++ day8/ts/main.ts
@@ -4,7 +4,6 @@ export default {
     part1(file: string): number {
         // Implementation
         let rows = file.split("\n")
-        let visible_trees = 0
         let previous_tree = 0
         let array: string[] = []
         let index = 0
@@ -13,16 +12,11 @@ export default {
         rows.forEach(row => {
             previous_tree = -1
             for (let i = 0; i < row.length; i++) {
-
                 if (previous_tree < parseInt(row[i])) {
                     if (array.indexOf(`${i},${index}`) === -1) {
                         array.push(`${i},${index}`)
                     }
-                    visible_trees++;
-                } 
-                // else {
-                //     break
-                // }
+                }
                 if (previous_tree < parseInt(row[i])) {
                     previous_tree = parseInt(row[i])
                 }
@@ -30,27 +24,18 @@ export default {
             index++
         });
 
-        // looking at it from the north
-        //correct
         index = 0
-
+        
+        // looking at it from the north
         rows.forEach(y_position => {
+
             previous_tree = -1
             for (let x_position = 0; x_position < y_position.length; x_position++) {
-                // detta blir vertical check
-                // console.log(`${index},${x_position}`);
-                // console.log(rows[x_position][index]);
-                
                 if (previous_tree < parseInt(rows[x_position][index])) {
-                    // console.log(y_position[x_position]);
-                    
                     if (array.indexOf(`${index},${x_position}`) === -1) {
                         array.push(`${index},${x_position}`)
                     }
                 }
-                // else {
-                //     break
-                // }
                 if (previous_tree < parseInt(rows[x_position][index])) {
                     previous_tree = parseInt(rows[x_position][index])
                 }
@@ -60,25 +45,15 @@ export default {
 
         index = rows[0].length - 1
 
-        // rotation = rotateData(rows)
-        // console.log(rotation);
         //looking from the south
         rows.forEach(y_position => {
             previous_tree = -1
             for (let x_position = y_position.length - 1; x_position >= 0; x_position--) {
-                // console.log(`${index},${x_position}`);
-                // console.log(rows[x_position][index]);
-                
                 if (previous_tree < parseInt(rows[x_position][index])) {
-                    // console.log(`${index},${x_position}`);
-                    // console.log(y_position[x_position]);
                     if (array.indexOf(`${index},${x_position}`) === -1) {
                         array.push(`${index},${x_position}`)
                     }
                 }
-                // else {
-                //     break
-                // }
                 if (previous_tree < parseInt(rows[x_position][index])) {
                     previous_tree = parseInt(rows[x_position][index])
                 }
@@ -87,15 +62,15 @@ export default {
         })
 
         index = rows[0].length - 1
-        
+
         //looking from the east
         let highest_tree = 0
         rows.forEach(y_position => {
             highest_tree = -1
             for (let x_position = y_position.length - 1; x_position >= 0; x_position--) {
                 if (highest_tree < parseInt(y_position[x_position])) {
-                    if (array.indexOf(`${x_position},${index}`) === -1) {
-                        array.push(`${x_position},${index}`)
+                    if (array.indexOf(`${x_position},${y_position.length - 1 - index}`) === -1) {
+                        array.push(`${x_position},${y_position.length - 1 - index}`)
 
                     }
                 }
@@ -106,7 +81,7 @@ export default {
             index--
         })
         // console.log(array);
-        
+
         return array.length
 
     },
